cc1e8b7e86503d6a00274e231c5fac643b3cf8c0
Navin Thamindu <navin.thamindu@gmail.com>
2020-03-23 13:19:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,148 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Toolbar from "./components/Toolbar/Toolbar";
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home/Home';
+import News2 from './components/News/News';
+import Community from './components/Community/Community';
+import Gameinfo from './components/Gameinfo/Gameinfo';
+import Gallery from './components/Gallery/Gallery';
+import Notfound from './components/Notfound/Notfound';
+
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+
+import LoadingScreen from 'react-loading-screen';
+
+import * as firebase from 'firebase';
+
+class App extends Component{
+
+
+  // ------------------------------------
+  constructor (props){
+    super(props);
+    this.state={apiResponse:"", loadr: true};
+    super();
+    this.state ={
+      speed: 10
+    }
+  
+
+  }
+
+
+  loadstate = true;
+
+  callAPI(){
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text ())
+      .then(res => this.setState({apiResponse: res}));
+  }
+
+  componentWillMount(){
+    this.callAPI();
+  }
+  // ------------------------------------
+
+  componentDidMount(){
+    this.setState({loadr: false});
+
+    const rootRef = firebase.database().ref().child('thretaweb');
+    const speedRef  = rootRef.child('speed');
+    speedRef.on('value', snap =>{
+      this.setState({
+        speed: snap.val()
+      });
+    });
+  }
+
+  state ={
+    sideDrawerOpen: false
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {sideDrawerOpen: !prevState.sideDrawerOpen};
+    });
+  };
+
+  backdropClickHandler =() =>{
+    this.setState({sideDrawerOpen: false});
+  }
+
+  render() {
+
+    let backdrop;
+
+    if (this.state.sideDrawerOpen){
+      
+      backdrop = <Backdrop click={this.backdropClickHandler}/>;
+    }
+    return (
+      <div>
+        <div>
+          <LoadingScreen
+              loading={this.state.loadr}
+              bgColor='#f1f1f1'
+              spinnerColor='#9ee5f8'
+              textColor='#676767'
+              logoSrc='/logo.png'
+              text='Here an introduction sentence (Optional)'
+            ></LoadingScreen>
+
+          <div style = {{height: '100%'}} className="App">
+            <Toolbar drawerClickHandler= {this.drawerToggleClickHandler}/>
+            <SideDrawer show={this.state.sideDrawerOpen}/>
+            {backdrop}
+            
+            <main style={{marginTop:'64px'}}>
+
+              <h1>{this.state.speed}</h1>
+              
+              
+            </main>
+
+          </div>
+          <LoadingScreen
+              loading={this.state.loadr}
+              bgColor='#f1f1f1'
+              spinnerColor='#9ee5f8'
+              textColor='#676767'
+              logoSrc='/logo.png'
+              text='Here an introduction sentence (Optional)'
+            >
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Home}></Route>
+                
+                <Route exact path="/News" component={News2} ></Route>
+                <Route exact path="/Community" component={Community}></Route>
+                <Route exact path="/Gallery" component={Gallery}></Route>
+                <Route exact path="/Gameinfo" component={Gameinfo}></Route>
+                <Route path="/" component={Notfound}></Route>
+
+              </Switch>
+            </Router>
+        
+          
+            </LoadingScreen>
+        </div>
+              {/* The core Firebase JS SDK is always required and must be listed first */}
+              <script src="/__/firebase/7.11.0/firebase-app.js"></script>
+
+              {/* TODO: Add SDKs for Firebase products that you want to use
+              https://firebase.google.com/docs/web/setup#available-libraries */}
+              <script src="/__/firebase/7.11.0/firebase-analytics.js"></script>
+
+              {/* Initialize Firebase */}
+              <script src="/__/firebase/init.js"></script>
+      </div>
+    );
+}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey:"AIzaSyCmhhYYNQUUnPqZgOEpQwlvejHfuEqirQw",
+    authDomain:"thretaweb.firebaseio.com",
+    databaseURL:"https://thretaweb.firebaseio.com",
+    storageBucket: "thretaweb.firebaseio.com",
+
+
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
